7451b4ba3261bbd73aa67d584bfa1d5ab072eee9
Keven-Zhu <zhukeven86@gmail.com>
2019-09-21 22:44:00 -0400

    added comments

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ time_elapsed = []
 
 print("Loading images...")
 
+#reads in the image paths from the csv
 with open("input.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     line_count = 0
@@ -20,6 +21,8 @@ with open("input.csv") as csv_file:
         if line_count != 0:
             img1_path.append(row[0])
             img2_path.append(row[1])
+
+            #loads in and stores the images
             img1.append(Image.open(row[0]))
             img2.append(Image.open(row[1]))
         line_count += 1
@@ -28,19 +31,26 @@ print("Images loaded")
 
 print("Calculating similarities...")
 for x in range(len(img1)):
+
+    #starts the timer for the calculation
     time_start = int(round(time.time() * 1000))
 
+    #calculates the hash for the pair of images
     hash1 = str(imagehash.dhash(img1[x]))
     hash2 = str(imagehash.dhash(img2[x]))
 
+    #calculates the hamming distance of the two hashes
     similarity.append((distance.hamming(hash1, hash2)) / len(hash1))
 
+    #ends the timer and records the time taken for the hashing function to run
     time_end = int(round(time.time() * 1000))
     time_elapsed.append(time_end - time_start)
 
 print("Done calculations")
 
 print("Writing to CSV file")
+
+#Writes to the csv file
 with open("output.csv", mode="w") as output_file:
     writer = csv.writer(output_file, delimiter=",")
 
